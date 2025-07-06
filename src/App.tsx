c70b3851f4310cabd3e1a4d8bc0ef53b237e8113
Utkarsh Raj Srivastava <utkarshrs002@gmail.com>
2025-07-07 00:45:53 +0530

    third commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import WeatherDashboard from './pages/WeatherDashboard'
 import CityPage from './pages/CityPage'
 
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient();
 
--- src/pages/WeatherDashboard.tsx
+++ src/pages/WeatherDashboard.tsx
@@ -1,6 +1,24 @@
+import { Button } from "@/components/ui/button"
+import { RefreshCw } from "lucide-react"
+
 const WeatherDashboard = () => {
  return (
-  <div>WeatherDashboard</div>
+  <div className="space-y-4"> 
+   {/* Favorite cities */}
+   <div className="flex items-center justify-between">
+    <div className="text-xl font-bold tracking-tight">
+     My location
+    </div>
+    <Button variant={'outline'}
+     size={'icon'}
+    // onClick={handleRefresh}
+    // disabled={} 
+    >
+     <RefreshCw className="h-4 w-4" />
+    </Button>
+   </div>
+   {/* current and hrly weather */}
+  </div>
  )
 }
 
