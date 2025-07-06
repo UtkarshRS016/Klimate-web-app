1a7434eb1609a1071c155e4a17a55b3179849268
Utkarsh Raj Srivastava <utkarshrs002@gmail.com>
2025-07-06 23:59:24 +0530

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Layout from './components/Layout'
-import { ThemeProvider } from './context/theme-provider'
+import { ThemeProvider } from './context/themeProvider'
+import WeatherDashboard from './pages/WeatherDashboard'
+import CityPage from './pages/CityPage'
+
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 function App() {
 
  return (
   <>
-   <BrowserRouter>
-    <ThemeProvider>
-     <Layout>
-      Hello
-     </Layout>
-    </ThemeProvider>
-   </BrowserRouter>
+   <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+     <ThemeProvider>
+      <Layout>
+       <Routes>
+        <Route path='/' element={<WeatherDashboard />}></Route>
+        <Route path='/city/:cityName' element={<CityPage />}></Route>
+       </Routes>
+      </Layout>
+     </ThemeProvider>
+    </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={false} />
+   </QueryClientProvider>
   </>
  )
 }
