82c935bbcb9fd257cd77fab57b0fa550def37d13
Utkarsh Raj Srivastava <utkarshrs002@gmail.com>
2025-07-06 15:52:35 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter } from 'react-router-dom'
+import './App.css'
+import Layout from './components/Layout'
+import { ThemeProvider } from './context/theme-provider'
+
+function App() {
+
+ return (
+  <>
+   <BrowserRouter>
+    <ThemeProvider>
+     <Layout>
+      Hello
+     </Layout>
+    </ThemeProvider>
+   </BrowserRouter>
+  </>
+ )
+}
+
+export default App
