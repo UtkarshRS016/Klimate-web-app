780c5501e51ba25e2407f60b05f3f466ad81c086
Utkarsh Raj Srivastava <utkarshrs002@gmail.com>
2025-07-19 14:40:21 +0530

    corrected header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import { useTheme } from "@/context/themeProvider"
 import { Moon, Sun } from "lucide-react";
 import { Link } from "react-router-dom"
@@ -11,17 +11,21 @@ const Header = () => {
 
  const [mounted, setMounted] = useState(false)
 
-  useEffect(() => {
-    setMounted(true)
-  }, [])
+ useEffect(() => {
+  setMounted(true)
+ }, [])
 
  return (
   <header className="sticky top-0 z-50 w-full border-b bg-background/60 backdrop-blur py-2 supports-[backdrop]:bg-background/60">
    <div className="container flex mx-auto h-16 items-center justify-between px-4 overflow-hidden">
     <Link to={'/'}>
-     <img src={isDark ? "/logo_01.png" : "/logo_02.png"}
-      alt="Klimate logo"
-      className="h-14" />
+     {mounted && (
+      <img
+       src={isDark ? "/logo_01.png" : "/logo_02.png"}
+       alt="Klimate logo"
+       className="h-14"
+      />
+     )}
     </Link>
 
     <div className="flex flex-nowrap max-w-full items-center gap-2 sm:gap-4">
@@ -30,12 +34,19 @@ const Header = () => {
      </div>
 
      {/* theme toggle */}
-     <div className={`flex items-center cursor-pointer transition-transform duration-500
-     ${isDark ? "rotate-180" : "rotate-0"}`}
-      onClick={() => setTheme(isDark ? "light" : "dark")}>
-      {isDark ? <Sun className="h-6 w-6 text-yellow-500 rotate-0 transition-all" /> :
-       <Moon className="h-6 w-6 text-blue-500 rotate-0 transition-all" />}
-     </div>
+     {mounted && (
+      <div
+       className={`flex items-center cursor-pointer transition-transform duration-500 ${isDark ? "rotate-180" : "rotate-0"
+        }`}
+       onClick={() => setTheme(isDark ? "light" : "dark")}
+      >
+       {isDark ? (
+        <Sun className="h-6 w-6 text-yellow-500 rotate-0 transition-all" />
+       ) : (
+        <Moon className="h-6 w-6 text-blue-500 rotate-0 transition-all" />
+       )}
+      </div>
+     )}
 
     </div>
    </div>
